621f95883c1f257f92169c4f62d28ad690976efc
serak <serakrzysztof@gmail.com>
2024-08-12 19:51:19 +0200

    32.5

--- frontend/src/main/java/com/kodilla/frontend/MainView.java
+++ frontend/src/main/java/com/kodilla/frontend/MainView.java
@@ -3,6 +3,7 @@ package com.kodilla.frontend;
 import com.kodilla.frontend.BookForm;
 import com.kodilla.frontend.domain.Book;
 import com.kodilla.frontend.service.BookService;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -18,22 +19,39 @@ public class MainView extends VerticalLayout {
     private TextField filter = new TextField();
     private BookForm form = new BookForm(this);
 
+    private Button addNewBook = new Button("Add new book");
+
     public MainView() {
         filter.setPlaceholder("Filter by title...");
         filter.setClearButtonVisible(true);
         filter.setValueChangeMode(ValueChangeMode.EAGER);
         filter.addValueChangeListener(e -> update());
-
         grid.setColumns("title", "author", "publicationYear", "type");
+
+        addNewBook.addClickListener(e -> {
+            grid.asSingleSelect().clear();
+            form.setBook(new Book());
+        });
+        HorizontalLayout toolbar = new HorizontalLayout(filter, addNewBook);
 
         HorizontalLayout mainContent = new HorizontalLayout(grid, form);
         mainContent.setSizeFull();
         grid.setSizeFull();
 
-        add(filter, mainContent);
+        add(toolbar, mainContent);
+        form.setBook(null);
         setSizeFull();
-
         refresh();
+
+
+        form.setBook(null);
+
+        grid.asSingleSelect().addValueChangeListener(event -> form.setBook(grid.asSingleSelect().getValue()));
+
+        addNewBook.addClickListener(e -> {
+            grid.asSingleSelect().clear();
+            form.setBook(new Book());
+        });
 
 
     }
